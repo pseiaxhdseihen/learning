1baacc1715ebb83198a23865f3009f473b9910b9
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-04-24 12:23:47 +0800

    api包初始化

---
+++ mingyue-springboot-api/src/main/java/com/csp/mingyue/api/MingYueSpringbootApiApplication.java
@@ -0,0 +1,12 @@
+package com.csp.mingyue.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingYueSpringbootApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MingYueSpringbootApiApplication.class, args);
+  }
+}
