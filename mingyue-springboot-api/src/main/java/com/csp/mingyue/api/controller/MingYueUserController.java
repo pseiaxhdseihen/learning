51b5f46c20cce02715788892b6d799b171b374df
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-04-26 12:26:49 +0800

    【接口篇】SpringBoot 快速实践 RESTful API 架构风格

---
+++ mingyue-springboot-api/src/main/java/com/csp/mingyue/api/controller/MingYueUserController.java
@@ -0,0 +1,43 @@
+package com.csp.mingyue.api.controller;
+
+import com.csp.mingyue.api.model.MingYueUser;
+import com.csp.mingyue.api.service.MingYueUserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/** @author Strive */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class MingYueUserController {
+
+  private final MingYueUserService mingYueUserService;
+
+  @GetMapping("/{userId}")
+  public ResponseEntity<MingYueUser> queryUserById(@PathVariable Long userId) {
+    return ResponseEntity.ok(mingYueUserService.queryUserById(userId));
+  }
+
+  @PostMapping
+  public ResponseEntity<Long> addUser(@RequestBody MingYueUser user) {
+    return ResponseEntity.ok(mingYueUserService.addUser(user));
+  }
+
+  @PutMapping
+  public ResponseEntity<String> updateUser(@RequestBody MingYueUser user) {
+    return ResponseEntity.ok(mingYueUserService.updateUser(user));
+  }
+
+  @DeleteMapping("/{userId}")
+  public ResponseEntity<String> deleteUser(@PathVariable Long userId) {
+    return ResponseEntity.ok(mingYueUserService.deleteUser(userId));
+  }
+}
---
+++ mingyue-springboot-api/src/main/java/com/csp/mingyue/api/service/MingYueUserService.java
@@ -0,0 +1,66 @@
+package com.csp.mingyue.api.service;
+
+import cn.hutool.core.map.MapUtil;
+import com.csp.mingyue.api.model.MingYueUser;
+import java.util.Map;
+import org.springframework.stereotype.Service;
+
+/** @author Strive */
+@Service
+public class MingYueUserService {
+
+  /** 模拟用户存储 */
+  private static final Map<Long, MingYueUser> USER_MAP = MapUtil.newHashMap();
+
+  static {
+    USER_MAP.put(1L, MingYueUser.builder().userId(1L).username("mingyue").build());
+  }
+
+  /**
+   * 根据用户ID查询用户信息
+   *
+   * @param userId 用户ID
+   * @return 用户信息
+   */
+  public MingYueUser queryUserById(Long userId) {
+    return USER_MAP.get(userId);
+  }
+
+  /**
+   * 添加用户
+   *
+   * @param user 用户信息
+   * @return 新用户ID
+   */
+  public Long addUser(MingYueUser user) {
+    USER_MAP.put(2L, user);
+
+    return user.getUserId();
+  }
+
+  /**
+   * 更新用户
+   *
+   * @param user 用户信息
+   */
+  public String updateUser(MingYueUser user) {
+    MingYueUser mingYueUser = USER_MAP.get(user.getUserId());
+
+    USER_MAP.put(user.getUserId(), user);
+
+    return mingYueUser.getUsername() + " 更新为：" + user.getUsername();
+  }
+
+  /**
+   * 根据用户ID删除用户
+   *
+   * @param userId 用户ID
+   */
+  public String deleteUser(Long userId) {
+    int size = USER_MAP.size();
+
+    USER_MAP.remove(userId);
+
+    return "原" + size + "用户，删除后还有" + USER_MAP.size();
+  }
+}
