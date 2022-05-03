7afd04fe1c7e959606d9ff86d22c6dd9ce1ffe20
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-05-03 12:55:26 +0800

    mingyue-springboot-swagger2

---
+++ mingyue-springboot-swagger2/src/main/java/com/csp/mingyue/swagger/MingYueSpringbootSwagger2Application.java
@@ -0,0 +1,12 @@
+package com.csp.mingyue.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingYueSpringbootSwagger2Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MingYueSpringbootSwagger2Application.class, args);
+  }
+}
