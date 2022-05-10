f5786e245bb38e57be0259ebb7dc32ff31d21b0e
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-05-10 13:17:53 +0800

    项目结构调整

---
+++ mingyue-springboot-swagger3/src/main/java/com/csp/mingyue/swagger3/MingYueSpringbootSwagger3Application.java
@@ -0,0 +1,12 @@
+package com.csp.mingyue.swagger3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingYueSpringbootSwagger3Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MingYueSpringbootSwagger3Application.class, args);
+  }
+}
