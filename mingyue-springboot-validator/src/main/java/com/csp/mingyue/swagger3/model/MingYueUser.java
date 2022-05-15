f395364c0eb859ed1e5681c197692466b1b5132c
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-05-15 13:43:07 +0800

    mingyue-springboot-validator

---
+++ mingyue-springboot-validator/src/main/java/com/csp/mingyue/swagger3/model/MingYueUser.java
@@ -0,0 +1,25 @@
+package com.csp.mingyue.swagger3.model;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/** @author Strive */
+@Data
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ApiModel(value = "用户实体类", description = "用户信息描述类")
+public class MingYueUser {
+
+  @ApiModelProperty(value = "用户id")
+  private Long userId;
+
+  @ApiModelProperty(value = "用户名")
+  private String username;
+}
