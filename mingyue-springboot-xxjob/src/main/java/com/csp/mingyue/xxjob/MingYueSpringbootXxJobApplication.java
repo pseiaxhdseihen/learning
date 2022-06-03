81ecd7f5b6e3d63a57c019aa6093a3c1f1356392
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-06-03 15:25:26 +0800

    xxjob初始化提交

---
+++ mingyue-springboot-xxjob/src/main/java/com/csp/mingyue/xxjob/MingYueSpringbootXxJobApplication.java
@@ -0,0 +1,13 @@
+package com.csp.mingyue.xxjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** @author Strive */
+@SpringBootApplication
+public class MingYueSpringbootXxJobApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MingYueSpringbootXxJobApplication.class, args);
+  }
+}
