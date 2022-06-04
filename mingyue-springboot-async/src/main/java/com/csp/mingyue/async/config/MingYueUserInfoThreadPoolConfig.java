e669fe125e058fb98c1cc87fee2d2089d1c9dcfd
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-06-04 15:29:29 +0800

    修改线程池配置

--- mingyue-springboot-async/src/main/java/com/csp/mingyue/async/config/MingYueUserInfoThreadPoolConfig.java
+++ mingyue-springboot-async/src/main/java/com/csp/mingyue/async/config/MingYueUserInfoThreadPoolConfig.java
@@ -30,7 +30,7 @@ public class MingYueUserInfoThreadPoolConfig {
     // 主线程等待子线程执行时间
     threadPoolTaskExecutor.setAwaitTerminationSeconds(60);
     // 线程名字前缀
-    threadPoolTaskExecutor.setThreadNamePrefix("user-info-");
+    threadPoolTaskExecutor.setThreadNamePrefix("user-info-thread-pool-executor-");
     // RejectedExecutionHandler:当pool已经达到max-size的时候，如何处理新任务
     // CallerRunsPolicy:不在新线程中执行任务，而是由调用者所在的线程来执行
     threadPoolTaskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
