2431b7a44172afa59a821ae07371c4d5d2b76782
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-06-08 15:43:56 +0800

    mysql初始化

---
+++ mingyue-springboot-mysql/src/main/java/com/csp/mingyue/mysql/MingYueSpringbootMysqlApplication.java
@@ -0,0 +1,12 @@
+package com.csp.mingyue.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingYueSpringbootMysqlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MingYueSpringbootMysqlApplication.class, args);
+  }
+}
