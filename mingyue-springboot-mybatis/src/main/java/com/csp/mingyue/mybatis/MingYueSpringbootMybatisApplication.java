47ba3b833eba8c45fe0b9f4d8aa88b0ee211d0e8
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-06-12 15:58:14 +0800

    mybatis初始化

---
+++ mingyue-springboot-mybatis/src/main/java/com/csp/mingyue/mybatis/MingYueSpringbootMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.csp.mingyue.mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** @author Strive */
+@SpringBootApplication
+public class MingYueSpringbootMybatisApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MingYueSpringbootMybatisApplication.class, args);
+  }
+}
