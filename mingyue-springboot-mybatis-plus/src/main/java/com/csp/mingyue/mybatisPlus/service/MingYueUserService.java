66877b1c7d987bc40b92df722f6c6f8d30ad7699
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-06-23 16:31:50 +0800

    日志

--- mingyue-springboot-mybatis-plus/src/main/java/com/csp/mingyue/mybatisPlus/service/MingYueUserService.java
+++ mingyue-springboot-mybatis-plus/src/main/java/com/csp/mingyue/mybatisPlus/service/MingYueUserService.java
@@ -3,9 +3,11 @@ package com.csp.mingyue.mybatisPlus.service;
 import com.csp.mingyue.mybatisPlus.mapper.SysUserMapper;
 import com.csp.mingyue.mybatisPlus.model.MingYueUser;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 /** @author Strive */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class MingYueUserService {
@@ -19,6 +21,7 @@ public class MingYueUserService {
    * @return 用户信息
    */
   public MingYueUser queryUserById(Long userId) {
+    log.info("根据用户ID查询用户信息");
     return sysUserMapper.selectById(userId);
   }
 }
