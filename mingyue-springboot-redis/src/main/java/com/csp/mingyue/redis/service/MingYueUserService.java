cea01ff939a1756d91313519c27dd94dfbaa8f91
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-06-27 16:47:03 +0800

    redis

--- mingyue-springboot-redis/src/main/java/com/csp/mingyue/redis/service/MingYueUserService.java
+++ mingyue-springboot-redis/src/main/java/com/csp/mingyue/redis/service/MingYueUserService.java
@@ -1,5 +1,7 @@
 package com.csp.mingyue.redis.service;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
 import com.csp.mingyue.redis.mapper.SysUserMapper;
 import com.csp.mingyue.redis.model.MingYueUser;
 import lombok.RequiredArgsConstructor;
@@ -14,6 +16,10 @@ public class MingYueUserService {
 
   private final SysUserMapper sysUserMapper;
 
+  private final RedisService redisService;
+
+  private static final String USER_REDIS_KEY = "user:info:";
+
   /**
    * 根据用户ID查询用户信息
    *
@@ -22,6 +28,24 @@ public class MingYueUserService {
    */
   public MingYueUser queryUserById(Long userId) {
     log.info("根据用户ID查询用户信息");
-    return sysUserMapper.selectById(userId);
+
+    // 查询缓存中是否有查询的数据
+    String userJson = redisService.get(USER_REDIS_KEY + userId);
+
+    // 缓存中没有数据则去数据库查询
+    if (StrUtil.isBlank(userJson)) {
+      log.info("缓存中没有数据则去数据库查询");
+      MingYueUser mingYueUser = sysUserMapper.selectById(userId);
+
+      // 查询的结果放入缓存
+      redisService.set(USER_REDIS_KEY + userId, JSONUtil.toJsonStr(mingYueUser));
+
+      // 返回查询到的数据
+      return mingYueUser;
+    }
+
+    log.info("缓存中有对应数据直接返回");
+    // 缓存中有对应数据直接返回
+    return JSONUtil.toBean(userJson, MingYueUser.class);
   }
 }
