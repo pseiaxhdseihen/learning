05566eace7194dbebd2baa56f6f75158edb9b49e
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-07-07 17:24:36 +0800

    websocket初始化提交

---
+++ mingyue-springboot-websocket/src/main/java/com/csp/mingyue/webSocket/MingYueSpringbootWebSocketApplication.java
@@ -0,0 +1,12 @@
+package com.csp.mingyue.webSocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingYueSpringbootWebSocketApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MingYueSpringbootWebSocketApplication.class, args);
+  }
+}
