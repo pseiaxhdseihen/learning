309e836e04ab23daa5df1054e98404ab05975a23
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-07-14 17:47:57 +0800

    spring boot admin 初始化提交

---
+++ mingyue-springboot-admin/src/main/java/com/csp/mingyue/admin/MingYueSpringbootAdminApplication.java
@@ -0,0 +1,12 @@
+package com.csp.mingyue.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingYueSpringbootAdminApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MingYueSpringbootAdminApplication.class, args);
+  }
+}
