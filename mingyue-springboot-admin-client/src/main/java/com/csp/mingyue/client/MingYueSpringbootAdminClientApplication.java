c67fe2d693005329c008342f1244f2b5e617351a
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-07-17 17:59:05 +0800

    spring boot admin client

---
+++ mingyue-springboot-admin-client/src/main/java/com/csp/mingyue/client/MingYueSpringbootAdminClientApplication.java
@@ -0,0 +1,12 @@
+package com.csp.mingyue.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MingYueSpringbootAdminClientApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MingYueSpringbootAdminClientApplication.class, args);
+  }
+}
