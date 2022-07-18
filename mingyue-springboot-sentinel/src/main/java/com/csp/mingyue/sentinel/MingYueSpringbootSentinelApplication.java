94950f4174de99854e55ee0c23213ecb683977f4
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-07-18 18:00:12 +0800

    sentinel 初始化提交

---
+++ mingyue-springboot-sentinel/src/main/java/com/csp/mingyue/sentinel/MingYueSpringbootSentinelApplication.java
@@ -0,0 +1,13 @@
+package com.csp.mingyue.sentinel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** @author Strive */
+@SpringBootApplication
+public class MingYueSpringbootSentinelApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MingYueSpringbootSentinelApplication.class, args);
+  }
+}
