7874690dec51ad201917668ad32203b73e362ed5
pseiaxhdseihen <pseiaxhdseihen@outlook.com>
2022-07-22 18:13:17 +0800

    Security 代码初始化

---
+++ mingyue-springboot-security/src/main/java/com/csp/mingyue/security/MingYueSpringbootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.csp.mingyue.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** @author Strive */
+@SpringBootApplication
+public class MingYueSpringbootSecurityApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MingYueSpringbootSecurityApplication.class, args);
+  }
+}
